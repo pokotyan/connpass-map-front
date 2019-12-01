e14496ddab42167e98957db0797195308c35c1d1
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2019-12-01 18:51:51 +0900

    modify css

--- src/pages/search/map/index.tsx
+++ src/pages/search/map/index.tsx
@@ -14,7 +14,7 @@ const MapContainer = compose(
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `800px` }} />,
+    containerElement: <div style={{ height: `100%` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
