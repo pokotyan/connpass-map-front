48267d8fbb5e174c524381bea3278c290a7b448c
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2019-12-14 15:19:14 +0900

    ページごとにOGPを変える

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,24 +5,28 @@ import Header from "./header";
 import List from "./list";
 import styles from "./style.module.scss";
 import Map from "./map";
+import { Helmet } from "../../components/helmet";
 
 export default () => {
   const connpassState = useSelector((state: AppState) => state.connpass);
   const { events } = connpassState;
 
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.mapContainer}>
-        <div className={styles.map}>
-          <Map events={events} />
-        </div>
-        {events.length ? (
-          <div className={styles.list}>
-            <List />
+    <>
+      <Helmet title="connpass map" />
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.mapContainer}>
+          <div className={styles.map}>
+            <Map events={events} />
           </div>
-        ) : null}
+          {events.length ? (
+            <div className={styles.list}>
+              <List />
+            </div>
+          ) : null}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
